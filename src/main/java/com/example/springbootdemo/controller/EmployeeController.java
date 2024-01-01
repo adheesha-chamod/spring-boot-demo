d0e8befa25311accd90ac900d39f7734f465142f
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-01-01 21:56:11 +0530

    Complete update and select

--- src/main/java/com/example/springbootdemo/controller/EmployeeController.java
+++ src/main/java/com/example/springbootdemo/controller/EmployeeController.java
@@ -6,12 +6,10 @@ import com.example.springbootdemo.service.EmployeeService;
 import com.example.springbootdemo.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -45,6 +43,56 @@ public class EmployeeController {
                 return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
 
             }
+        } catch (Exception err) {
+            responseDTO.setCode(VarList.RES_ERROR);
+            responseDTO.setMessage(err.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/employee")
+    public ResponseEntity updateEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        try {
+            String res = employeeService.updateEmployee(employeeDTO);
+
+            if(res.equals("00")) {
+                responseDTO.setCode(VarList.RES_SUCCESS);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(null);
+                return new ResponseEntity(responseDTO, HttpStatus.CREATED);
+
+            } else if(res.equals("01")) {
+                responseDTO.setCode(VarList.RES_DUPLICATED);
+                responseDTO.setMessage("Employee not found");
+                responseDTO.setContent(null);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+
+            } else {
+                responseDTO.setCode(VarList.RES_FAIL);
+                responseDTO.setMessage("Error");
+                responseDTO.setContent(null);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+
+            }
+        } catch (Exception err) {
+            responseDTO.setCode(VarList.RES_ERROR);
+            responseDTO.setMessage(err.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/employee")
+    public ResponseEntity getAllEmployees() {
+        try {
+            List<EmployeeDTO> employeeDTOList = employeeService.getAllEmployees();
+
+            responseDTO.setCode(VarList.RES_SUCCESS);
+            responseDTO.setMessage("Success");
+            responseDTO.setContent(employeeDTOList);
+            return new ResponseEntity(responseDTO, HttpStatus.CREATED);
+
         } catch (Exception err) {
             responseDTO.setCode(VarList.RES_ERROR);
             responseDTO.setMessage(err.getMessage());
--- src/main/java/com/example/springbootdemo/service/EmployeeService.java
+++ src/main/java/com/example/springbootdemo/service/EmployeeService.java
@@ -5,9 +5,13 @@ import com.example.springbootdemo.entity.Employee;
 import com.example.springbootdemo.repo.EmployeeRepo;
 import com.example.springbootdemo.util.VarList;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -24,5 +28,19 @@ public class EmployeeService {
             employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
             return VarList.RES_SUCCESS;
         }
+    }
+
+    public String updateEmployee(EmployeeDTO employeeDTO) {
+        if(employeeRepo.existsById(employeeDTO.getId())) {
+            employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
+            return VarList.RES_SUCCESS;
+        } else {
+            return VarList.RES_NO_DATA_FOUND;
+        }
+    }
+
+    public List<EmployeeDTO> getAllEmployees() {
+        List<Employee> employeeList = employeeRepo.findAll();
+        return modelMapper.map(employeeList, new TypeToken<ArrayList<EmployeeDTO>>(){}.getType());
     }
 }
